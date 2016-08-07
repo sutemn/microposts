108ac174d1b8a53642dcbf99bf18a92b68fb6712
sutemn <sute.mn@gmail.com>
2016-08-07 14:40:44 +0000

    add sign sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  
+  def show # 追加
+   @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # ここを修正
     else
       render 'new'
     end
